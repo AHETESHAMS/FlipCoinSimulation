130e55d3d67afa5cbe3fc9c4c0bfd9b7b5308ce3
AHETESHAMS <aheteshams007@gmail.com>
2020-01-09 11:25:24 +0530

    Pushing second case with singlet combination in dictionary with percentage

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -5,16 +5,16 @@ tail=0
 i=0
 while [ $i  -lt 10 ]
 do
-randomCheck=$((RANDOM%2))
-if [[ $randomCheck -ge 1 ]]
-then
-	(( head++ ))
-	results[H]=$head
-else
-        (( tail++ ))
-	results[T]=$tail
-fi;
-(( i++ ))
+	randomCheck=$((RANDOM%2))
+	if [[ $randomCheck -ge 1 ]]
+	then
+		(( head++ ))
+		results[H]=$head
+	else
+   	(( tail++ ))
+		results[T]=$tail
+	fi;
+	(( i++ ))
 done
 percentageOfHead=$(( head*100/10 ))
 percentageOfTail=$(( tail*100/10 ))
