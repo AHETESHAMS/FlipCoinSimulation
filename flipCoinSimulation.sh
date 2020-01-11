00a6058baf87037713748a52f45614fe2a8ffa7a
AHETESHAMS <aheteshams007@gmail.com>
2020-01-11 11:47:31 +0530

    Pushing doublet combination of Head and tail with percentage

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,21 +1,38 @@
 #!/bin/bash -x
 declare -A results
-head=0
-tail=0
+doubleHead=0
+doubleTail=0
+headTail=0
+tailHead=0
 i=0
 while [ $i  -lt 10 ]
 do
-	randomCheck=$((RANDOM%2))
-	if [[ $randomCheck -ge 1 ]]
+	randomCheck=$((RANDOM%4))
+	echo $randomCheck
+	if [[ $randomCheck -eq 0 ]]
 	then
-		(( head++ ))
-		results[H]=$head
+		(( doubleHead++ ))
+		results[HH]=$doubleHea
+	elif [[ $randomCheck -eq 1 ]]
+	then
+   	(( doubleTail++ ))
+		results[TT]=$doubleTail 
+	elif [[ $randomCheck -eq 2 ]]
+	then
+		(( headTail++ ))
+		results[HT]=$headTail
 	else
-   	(( tail++ ))
-		results[T]=$tail
+		(( tailHead++ ))
+		results[TH]=$tailHead
 	fi;
 	(( i++ ))
 done
-percentageOfHead=$(( head*100/10 ))
-percentageOfTail=$(( tail*100/10 ))
-echo "Percentage Of Head is "$percentageOfHead
+percentageOfDoubleHead=$(( doubleHead*100/10 ))
+percentageOfDoubleTail=$(( doubleTail*100/10 ))
+percentageOfHeadTail=$(( headTail*100/10 ))
+percentageOfTailHead=$(( tailHead*100/10 ))
+echo "Percentage Of Head Head is "$percentageOfDoubleHead
+echo "Percentage Of Tail Tail is "$percentageOfDoubleTail
+echo "Percentage Of Head Tail is "$percentageOfHeadTail
+echo "Percentage Of Tail Head is "$percentageOfTailHead
+
